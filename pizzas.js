35acd3863ca661bd97b8f104b46dfce24afc4cb5
Felipe Machado <feelipe@email.com>
2019-12-18 16:40:47 -0300

    corrigido tamanho da pizza

--- pizzas.js
+++ pizzas.js
@@ -4,7 +4,7 @@ let pizzaJson = [
     name: "Mussarela",
     img: "images/pizza.png",
     price: 20.19,
-    sizes: ["100g", "530g", "860g"],
+    sizes: ["320g", "530g", "860g"],
     description:
       "Descrição da pizza em mais de uma linha muito legal bem interessante"
   },
